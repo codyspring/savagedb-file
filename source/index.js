389a434c70448a5fd68e9f917613a37f03f19d6b
Cody Spring <codyspring@me.com>
2017-04-03 03:39:44 -0700

    export for use in SavageDB

--- source/index.js
+++ source/index.js
@@ -1,1 +1,27 @@
-module.exports = {};
+const Database = require('./database');
+const Collection = require('./collection');
+const Document = require('./document');
+
+module.exports = options => (database) => {
+  // Once SavageDB tells us to persist a database, create it with the name we get.
+  Database.create(database.name, options.location);
+
+  // When SavageDB emits a collection, create it and subscribe to document events.
+  database.subject('collection-created').subscribe((name) => {
+    let location = `${options.location}/${database.name}`;
+    Collection.create(name, location);
+
+    location = `${location}/${name}`;
+    database.collections[name].subject('document-inserted').subscribe((doc) => {
+      Document.save(doc, location, options.fileType);
+    });
+
+    database.collections[name].subject('document-updated').subscribe((doc) => {
+      Document.save(doc, location, options.fileType);
+    });
+
+    database.collections[name].subject('document-deleted').subscribe((id) => {
+      Document.delete(id, location);
+    });
+  });
+};
