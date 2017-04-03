c1e9ee6b23a05679f8d0b0673b6914eaf43acd89
Cody Spring <codyspring@me.com>
2017-04-03 04:15:14 -0700

    yaml document operations

--- source/document.js
+++ source/document.js
@@ -1,12 +1,14 @@
 const fs = require('graceful-fs');
 const yaml = require('js-yaml');
 
-const Save = (data, location) => new Promise((pass) => {
-  fs.writeFile(`${location}/${data.id}.json`, JSON.stringify(data), () => pass());
+const Save = (data, location, fileType = 'json') => new Promise((pass) => {
+  let parsed = JSON.stringify(data);
+  if (fileType === 'yaml') parsed = yaml.safeDump(data);
+  fs.writeFile(`${location}/${data.id}.${fileType}`, parsed, () => pass());
 });
 
-const Delete = (id, location) => new Promise((pass) => {
-  fs.unlink(`${location}/${id}.json`, () => pass());
+const Delete = (id, location, fileType = 'json') => new Promise((pass) => {
+  fs.unlink(`${location}/${id}.${fileType}`, () => pass());
 });
 
 module.exports = {
