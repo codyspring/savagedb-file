cb1b6d1eaa6afa913301a3767d25b7663d22d442
Cody Spring <codyspring@me.com>
2017-04-04 18:16:17 -0700

    tests

--- __tests__/database.js
+++ __tests__/database.js
@@ -1,5 +1,6 @@
 const mkdirp = require('mkdirp');
 const fs = require('graceful-fs');
+const yaml = require('js-yaml');
 const Database = require('../source/database');
 
 const location = './data-test/database';
@@ -17,8 +18,37 @@ describe('::Database', () => {
     });
   });
 
+  describe('#load()', () => {
+    beforeAll(() => {
+      // Make databases and collections.
+      mkdirp.sync(`${location}/load-json/foo`);
+      mkdirp.sync(`${location}/load-json/bar`);
+      mkdirp.sync(`${location}/load-yaml/foo`);
+
+      // Write some example files.
+      fs.writeFileSync(`${location}/load-json/foo/json1.json`, JSON.stringify({ id: 'json1', foo: 1 }));
+      fs.writeFileSync(`${location}/load-json/foo/json2.json`, JSON.stringify({ id: 'json2', foo: 2 }));
+      fs.writeFileSync(`${location}/load-json/bar/json1.json`, JSON.stringify({ id: 'json1', foo: 3 }));
+      fs.writeFileSync(`${location}/load-yaml/foo/yaml1.yaml`, yaml.safeDump({ id: 'yaml1', foo: 4 }));
+    });
+
+    it('should recursively load data from collections', () => {
+      const db = { name: 'load-json', collections: {} };
+      Database.load(db, location, 'json');
+      expect(db.collections.foo.json1.foo).toBe(1);
+      expect(db.collections.foo.json2.foo).toBe(2);
+      expect(db.collections.bar.json1.foo).toBe(3);
+    });
+
+    it('should load yaml when specified', () => {
+      const db = { name: 'load-yaml', collections: {} };
+      Database.load(db, location, 'yaml');
+      expect(db.collections.foo.yaml1.foo).toBe(4);
+    });
+  });
+
   afterAll(() => {
-    fs.rmdirSync(location);
-    fs.rmdirSync('./data-test');
+    fs.unlinkSync(location);
+    fs.unlinkSync('./data-test');
   });
 });
