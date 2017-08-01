b751c95584934877af12f8dcdf725b78dc6f4a50
Cody Spring <codyspring@me.com>
2017-07-31 22:16:02 -0700

    standard tests

--- source/document.js
+++ source/document.js
@@ -1,14 +1,14 @@
 const fs = require('graceful-fs')
 const yaml = require('js-yaml')
 
-const Save = (data, location, fileType = 'json') => new Promise((pass) => {
+const Save = (data, location, fileType = 'json') => new Promise((resolve, reject) => {
   let parsed = JSON.stringify(data)
   if (fileType === 'yaml') parsed = yaml.safeDump(data)
-  fs.writeFile(`${location}/${data.id}.${fileType}`, parsed, () => pass())
+  fs.writeFile(`${location}/${data.id}.${fileType}`, parsed, () => resolve())
 })
 
-const Delete = (id, location, fileType = 'json') => new Promise((pass) => {
-  fs.unlink(`${location}/${id}.${fileType}`, () => pass())
+const Delete = (id, location, fileType = 'json') => new Promise((resolve, reject) => {
+  fs.unlink(`${location}/${id}.${fileType}`, () => resolve())
 })
 
 module.exports = {
